18663c873363e786979927fdc0b95f021067640a
Vinicius <viniciusoliveira926@gmail.com>
2021-11-29 10:48:31 -0300

    Add estrutura e foto

--- flutter_desafio_ui_01/lib/aluno.dart
+++ flutter_desafio_ui_01/lib/aluno.dart
@@ -11,7 +11,25 @@ class _AlunoState extends State<Aluno> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.blue.shade300,
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 90),
+        child: Column(
+          children: [
+            const Expanded(
+              child:Align(
+                alignment: Alignment.topCenter,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 85,
+                  backgroundImage: NetworkImage("https://imgur.com/EPUWxPP.png")
+                ),
+              ),
+            ),
+            
+          ],
+        ),
+      ),
     );
   }
 }
