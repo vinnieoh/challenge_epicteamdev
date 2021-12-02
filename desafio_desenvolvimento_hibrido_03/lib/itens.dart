37d4d898dcf0ee620c8ba8e1d7a490c74cf41555
Vinicius <viniciusoliveira926@gmail.com>
2021-12-02 15:06:02 -0300

    commit inicial

---
+++ desafio_desenvolvimento_hibrido_03/lib/itens.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class ItensLista extends StatefulWidget {
+  const ItensLista({ Key? key }) : super(key: key);
+
+  @override
+  _ItensListaState createState() => _ItensListaState();
+}
+
+class _ItensListaState extends State<ItensLista> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ desafio_desenvolvimento_hibrido_03/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:desafio_desenvolvimento_hibrido_03/itens.dart';
+import 'package:flutter/material.dart';
+import 'package:desafio_desenvolvimento_hibrido_03/itens.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: ItensLista()
+  ));
+}
